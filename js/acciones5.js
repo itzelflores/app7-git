857b0c391982e6f45b05cc343846fa38d3f5a429
itzelflores <cb16.flores.aracen.itzel@gmail.com>
2019-05-14 09:55:34 -0500

    6

--- js/acciones5.js
+++ js/acciones5.js
@@ -1,13 +1,28 @@
-// JavaScript Document
+//JavaScript Document
 
 $(document).ready(function(e){
 	document.addEventListener("deviceready",function(){
-		$('#disp table td').eq(3).text(device.model);
-		$('#disp table td').eq(5).text(device.cordova);
-		$('#disp table td').eq(7).text(device.platform);
-		$('#disp table td').eq(9).text(device.version);
-		$('#disp table td').eq(11).text(device.uuid);
-	},false);//ready device
-	
-});//document
 		
+		$('#izquierda').on("swipeleft",function(){
+			navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion ","Aceptar"});
+		});
+		
+	$('#derecha').on("swiperight",function(){
+			navigator.notification.confirm("¿Qué quieres hacer?",function(opt){
+				switch(opt)	
+			{
+				case 1:
+				navigator.notification.beep(1);
+				break;
+				
+				case 1:
+				navigator.notification.vibrate(1000);
+				break;
+			}
+			},"Aplicacion","Beep,Vibrar,Cancelar");
+			
+	});
+	},false);
+});
+				
+				
